e2f8134e11041cd5beb04df20a31a5bb303d32b6
erw1nhmtz <erw1nhmtz@gmail.com>
2023-04-11 17:51:39 +0300

    Added files for task5.

---
+++ Lab_2/task5/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class SumOfIntegers {
+private:
+    int data;
+    static int sum;
+
+public:
+    SumOfIntegers(int value) : data(value) {
+        sum += data;
+    }
+
+    int getData() const {
+        return data;
+    }
+
+    static int getSum() {
+        return sum;
+    }
+};
+
+int SumOfIntegers::sum = 0;
+
+int main() {
+    SumOfIntegers a(5);
+    SumOfIntegers b(10);
+    SumOfIntegers c(15);
+
+    std::cout << "Sum of integers: " << SumOfIntegers::getSum() << std::endl;
+
+    return 0;
+}
